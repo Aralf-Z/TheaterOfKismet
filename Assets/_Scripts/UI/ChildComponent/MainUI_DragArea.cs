f66916bd9124477588fc4edf2bebe4435ec1a8c8
CowdogEin <candats@outlook.com>
2024-07-17 00:18:24 +0800

    好的，Drag正式版，因为UGUI事件接口必须要挂一个UI组件

--- Assets/_Scripts/UI/ChildComponent/MainUI_DragArea.cs
+++ Assets/_Scripts/UI/ChildComponent/MainUI_DragArea.cs
@@ -9,7 +9,6 @@ namespace TheaterOfKismet.UI
 	,IBeginDragHandler
 	,IDragHandler
 	,IEndDragHandler
-	,IPointerClickHandler
 	{
 		private MainModel mMainModel;
 
@@ -19,28 +18,9 @@ namespace TheaterOfKismet.UI
 		{
 			mMainModel = this.GetModel<MainModel>();
 		}
-
-		private void Update()
-		{
-			if (Input.GetMouseButton(0))
-			{
-				var mousePos = Input.mousePosition;
-
-				mMainModel.dragDirection.Value = mousePos.x - mPreMousePos.x;
-
-				mPreMousePos = mousePos;
-			}
-			else
-			{
-				mMainModel.dragDirection.Value = 0;
-			}
-		}
-
 		
-		//下面3个接口我不知道为什么不能用，很奇怪
 		public void OnBeginDrag(PointerEventData eventData)
 		{
-			Debug.Log("start");
 			if (eventData.IsMouseLeft())
 			{
 				mMainModel.dragDirection.Value = 0;
@@ -51,7 +31,6 @@ namespace TheaterOfKismet.UI
 
 		public void OnDrag(PointerEventData eventData)
 		{
-			Debug.Log("dragging");
 			if (eventData.IsMouseLeft())
 			{
 				var mousePos = Input.mousePosition;
@@ -64,16 +43,10 @@ namespace TheaterOfKismet.UI
 
 		public void OnEndDrag(PointerEventData eventData)
 		{
-			Debug.Log("end");
 			if (eventData.IsMouseLeft())
 			{
 				mMainModel.dragDirection.Value = 0;
 			}
-		}
-
-		public void OnPointerClick(PointerEventData eventData)
-		{
-			Debug.Log("onclick");
 		}
 	}
 }
